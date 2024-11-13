89e2a1bb20ff2cea5f654ca6a9576be37a00f636
praveen-1916 <pkumarbattu@gmail.com>
2024-11-13 20:05:57 +0530

    alert bug fixing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,15 +6,32 @@ import TaskState from './context/TaskState';
 import Login from './components/Login';
 import SignUp from './components/SignUp';
 import Footer from './components/Footer';
-// import TaskDetail from './components/TaskDetail';
+import { useContext } from 'react';
+import TaskContext from './context/TaskContext';
 
 
 function App() {
+
+  const context = useContext(TaskContext);
+  const { alertData } = context;
+
+
 
   return (
     <>
       <Router>
         <TaskState>
+          {alertData && <Alert className={alertData.success ? 'w-max shadow-lg shadow-[#bdfdc7] bg-[#f0fff1] text-[#2ec945] border-l-4 border-l-[#2ec946] px-5 z-50 rounded-sm fixed bottom-5 right-5' : 'w-max shadow-xl shadow-[#fdb9b9] bg-[#fdf0d5] text-[#d00000] border-l-4 border-l-[#d00000] px-5 z-50 rounded-sm fixed bottom-5 right-5'}
+            icon={React.createElement(alertData.icon, {
+              className: `h-6 w-6`,
+            })}
+            animate={{
+              mount: { y: 0 },
+              unmount: { y: 100 },
+            }}
+          >
+            {alertData.msg}
+          </Alert>}
           <Routes>
             <Route exact path='/login' element={<Login />} />
             <Route exact path='/signup' element={<SignUp />} />
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -45,17 +45,6 @@ function Home() {
     return (
         <>
             <LoadingBar progress={progress} color='#1a237e' />
-            {alertData && <Alert className={alertData.success ? 'w-max shadow-lg shadow-[#bdfdc7] bg-[#f0fff1] text-[#2ec945] border-l-4 border-l-[#2ec946] px-5 z-50 rounded-sm fixed bottom-5 right-5' : 'w-max shadow-xl shadow-[#fdb9b9] bg-[#fdf0d5] text-[#d00000] border-l-4 border-l-[#d00000] px-5 z-50 rounded-sm fixed bottom-5 right-5'}
-                icon={React.createElement(alertData.icon, {
-                    className: `h-6 w-6`,
-                })}
-                animate={{
-                    mount: { y: 0 },
-                    unmount: { y: 100 },
-                }}
-            >
-                {alertData.msg}
-            </Alert>}
             <div className='flex'>
                 {!openNav && <SideBarSimple />}
                 <div className='flex-grow bg-gray-50'>
